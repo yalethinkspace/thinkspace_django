e9cce93e51bb42bd55a2afd0b40ad4edddef3731
Sarim Abbas <sarim.stays@gmail.com>
2018-08-06 01:19:44 +0100

    password reset functionality

--- client/forms.py
+++ client/forms.py
@@ -11,3 +11,7 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
+# class DashboardBasicInfo(forms.Form):
+#     first_name = 
+#     last_name = 
