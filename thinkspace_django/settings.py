f11c1e68040e82ebdaa3910f4e66639e77cae6df
Sarim Abbas <sarim.stays@gmail.com>
2018-08-07 02:30:20 +0100

    static test

--- thinkspace_django/settings.py
+++ thinkspace_django/settings.py
@@ -120,9 +120,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
