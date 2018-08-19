ea021ea2847a812330b77f6c9bdc0e5c104c48e7
Sarim Abbas <sarim.stays@gmail.com>
2018-08-18 22:30:37 -0400

    full news pages

--- client/urls.py
+++ client/urls.py
@@ -13,7 +13,8 @@ urlpatterns = [
     path('dashboard/change_password/', views.change_password, name='change_password'),
     path('dashboard/messages/', views.dashboard_messages_conversation_list, name='dashboard_messages_conversation_list'),
     path('dashboard/messages/<int:conversation_id>/', views.dashboard_messages_conversation, name='dashboard_messages_conversation'),
-    path('news/', views.news, name='news')
+    path('news/', views.news, name='news'),
+    path('news/<int:post_id>/', views.news_item, name='news_item'),
 ]
  
    
--- client/views.py
+++ client/views.py
@@ -208,6 +208,12 @@ def news(request):
     paginator = Paginator(posts, 1)  # show 5 posts per page
     page = request.GET.get('page')
     posts = paginator.get_page(page)
-    return render(request, 'news/news.html', {
+    return render(request, 'news/news_list.html', {
         "posts" : posts,
+    })
+
+def news_item(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    return render(request, 'news/news_item.html', {
+        "post" : post,
     })
